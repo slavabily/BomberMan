305c0dd1a70b23580485fa61c5bc40a3e5540aac
slava bily <slavabily@me.com>
2021-11-20 16:57:08 +0200

    the initial bombs batch is created along with planted field on the second second

--- BomberMan/main.swift
+++ BomberMan/main.swift
@@ -8,7 +8,26 @@
 import Foundation
 
 func bomberMan(n: Int, grid: [String]) -> [String] {
-     
+    var fs = [[Character]]()
+    for i in grid {
+        fs.append(Array(i))
+    }
+    print(fs)
+    var bombsBatch = [(Int, Int)]()
+    for i in 0..<fs.count {
+        for j in 0..<fs[i].count {
+            if fs[i][j] == "O" {
+                let pos = (i: i, j: j)
+                bombsBatch.append(pos)
+            }
+        }
+    }
+    print(bombsBatch)
+    var ss = [[Character]]()
+    for i in fs {
+        ss.append(Array(repeating: "O", count: i.count))
+    }
+    print(ss)
     return [""]
 }
 
