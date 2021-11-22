32020b9635a3d6f082ed68de2b24b8cbe34a53ed
slava bily <slavabily@me.com>
2021-11-22 09:18:41 +0200

    the horizontal neighbours explosion flow is made

--- BomberMan/main.swift
+++ BomberMan/main.swift
@@ -8,26 +8,28 @@
 import Foundation
 
 func bomberMan(n: Int, grid: [String]) -> [String] {
-    var fs = [[Character]]()
+    var ss = [[Character]]()
     for i in grid {
-        fs.append(Array(i))
+        let j = i.replacingOccurrences(of: ".", with: "b")
+        ss.append(Array(j))
+        print(Array(j))
     }
-    print(fs)
-    var bombsBatch = [(Int, Int)]()
-    for i in 0..<fs.count {
-        for j in 0..<fs[i].count {
-            if fs[i][j] == "O" {
-                let pos = (i: i, j: j)
-                bombsBatch.append(pos)
+    print("\n")
+    print(ss)
+    print("\n")
+    for i in 0..<ss.count {
+        for j in 0..<ss[i].count {
+            if ss[i][j] == "O" {
+                ss[i][j] = "."
+                if j > 0 && j < ss[i].count - 1 {
+                    ss[i][j + 1] = "."
+                    ss[i][j - 1] = "."
+                } 
             }
         }
+        print(ss[i])
     }
-    print(bombsBatch)
-    var ss = [[Character]]()
-    for i in fs {
-        ss.append(Array(repeating: "O", count: i.count))
-    }
-    print(ss)
+    
     return [""]
 }
 
