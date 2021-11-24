6ee07d7b221b64d3d7e6aac84017ba3db3df479f
slava bily <slavabily@me.com>
2021-11-24 09:08:28 +0200

    explosion flow is improved but still with some bugs in edges points

--- BomberMan/main.swift
+++ BomberMan/main.swift
@@ -15,23 +15,21 @@ func bomberMan(n: Int, grid: [String]) -> [String] {
         print(Array(j))
     }
     print("\n")
-    print(ss)
-    print("\n")
     for i in 0..<ss.count {
         for j in 0..<ss[i].count {
             if ss[i][j] == "O" {
                 ss[i][j] = "."
-                if i > 0 && i < ss.count - 1 && j > 0 && j < ss[i].count - 1 {
+                if i > 0 && i < ss.count - 1 {
                     ss[i - 1][j] = "."
                     ss[i + 1][j] = "."
+                }
+                if j > 0 && j < ss[i].count - 1 {
                     ss[i][j + 1] = "."
                     ss[i][j - 1] = "."
                 }
             }
         }
     }
-    print("\n")
-    print(ss)
     print("\n")
     for i in ss {
         print(i)
